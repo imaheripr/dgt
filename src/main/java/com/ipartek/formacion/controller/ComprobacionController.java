896c847cc4015550660e4418560012ae60536f60
davidferrero <davidferreropeon@gmail.com>
2019-01-08 13:39:35 +0100

    servlet comprobar matricula

--- src/main/java/com/ipartek/formacion/controller/ComprobacionController.java
+++ src/main/java/com/ipartek/formacion/controller/ComprobacionController.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class ListarController
  */
-@WebServlet("/privado/listar")
+@WebServlet("/privado/comprobacion")
 public class ComprobacionController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
