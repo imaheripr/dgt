1353db44cf99d5de87c2fd4884821d21012d887c
davidferrero <davidferreropeon@gmail.com>
2019-01-16 13:54:01 +0100

    elimino variable de listar controller que utilizabamos para harcodear agente y que ya no utilizamos

--- src/main/java/com/ipartek/formacion/controller/ListarController.java
+++ src/main/java/com/ipartek/formacion/controller/ListarController.java
@@ -22,7 +22,7 @@ import com.ipartek.formacion.pojos.Multa;
 @WebServlet("/privado/listar")
 public class ListarController extends HttpServlet {
 
-	private static final int ID_AGENTE_PREDEFINIDO = 4;
+
 
 	private static final String LISTADO_MULTAS = "listadoMultas.jsp";
 	private static final String LISTADO_MULTAS_ANULAR = "listadoMultasAnuladas.jsp";
