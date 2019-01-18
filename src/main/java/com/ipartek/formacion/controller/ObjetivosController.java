139fa1a0e984d38fa3cec843cbfd819ad7a89831
davidferrero <davidferreropeon@gmail.com>
2019-01-18 10:04:47 +0100

    objetivos controller

--- src/main/java/com/ipartek/formacion/controller/ObjetivosController.java
+++ src/main/java/com/ipartek/formacion/controller/ObjetivosController.java
@@ -13,9 +13,12 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.log4j.Logger;
 
 import com.ipartek.formacion.daos.AgenteDAO;
+import com.ipartek.formacion.daos.CocheDAO;
 import com.ipartek.formacion.daos.MultaDAO;
+import com.ipartek.formacion.daos.ObjetivoDAO;
 import com.ipartek.formacion.pojos.Agente;
 import com.ipartek.formacion.pojos.Multa;
+import com.ipartek.formacion.pojos.Objetivo;
 
 
 
@@ -25,11 +28,13 @@ public class ObjetivosController extends HttpServlet {
 	//LOG
 		private final static Logger LOG = Logger.getLogger(ObjetivosController.class);
 
+		private ObjetivoDAO objetivoDAO = null;
+		private Objetivo objetivo = null;
 		
 		@Override
 	    public void init(ServletConfig config) throws ServletException {    
 	    	super.init(config);
-	    		
+	    	objetivoDAO = ObjetivoDAO.getInstance();	
 	    }
 		
 
@@ -40,11 +45,18 @@ public class ObjetivosController extends HttpServlet {
 	
 	
 	private void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-			
-		
-		
-	}
 	
-
+		String id_agente = request.getParameter("id_agente");
+		
+		Long id = Long.parseLong(id_agente);
+		
+		objetivo = objetivoDAO.objetivoMesActual(id); 
+		
+		request.setAttribute("objetivo", objetivo);												// si el objeto coche contiene matricula
+		
+		request.getRequestDispatcher("objetivos.jsp").forward(request, response);
+			
+	}
 }
+	
+	
